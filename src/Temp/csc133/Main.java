e2d8594b389cea988ff1d61e5d6e3ca5bcba8fe2
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-03-25 21:19:51 -0700

    Added Texture / new Camera files
    -last commit didnt include correct files

---
+++ src/Temp/csc133/Main.java
@@ -0,0 +1,12 @@
+package csc133;
+
+import static SlRenderer.slSingleBatchRenderer.render;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        render();
+    } // public static void main(String[] args)
+
+    
+} // public class Main
